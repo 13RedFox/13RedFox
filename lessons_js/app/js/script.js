5d53963b4a8d7d26e96c0446596904de8f53e892
CunningFox <sergei181989@gmail.com>
2019-04-01 12:30:53 +0300

    reLoaded all file

---
+++ lessons_js/app/js/script.js
@@ -0,0 +1,34 @@
+;
+(function IIFE() {
+	// let admin, name;
+	// name = 'Vasya';
+	// admin = name;
+	// console.log(`admin: ${admin}`);
+
+	// const ourPlanet = 'Earth';
+	// const userName = 'Petya';
+
+	// const userName = prompt('Type your name', 'User Name');
+
+	// const el = document.createElement('h1');
+	// el.innerText = userName;
+	// document.body.appendChild(el);
+
+	// const forma = document.getElementById('form');
+	// const textField = document.createElement('div');
+	// document.body.appendChild(textField);
+
+	// form.addEventListener('submit', function (event) {
+	// 	event.preventDefoult();
+
+	// 	const userName = form.elements.userName.value;
+	// 	const userPhone = form.elements.userPhone.value;
+
+	// 	textField.innerText = `${userName} ${userPhone}`;
+	// })
+
+
+	const random = Math.random();
+	console.log(random);
+
+})();
