145702b495ebda903fa2d9ff99db62b18babb37b
CunningFox <sergei181989@gmail.com>
2019-04-14 23:01:52 +0300

    add new block quote & fixed bug f_s-menu, akardeon and more...

--- mogo/app/js/script.js
+++ mogo/app/js/script.js
@@ -1,5 +1,5 @@
-$(document).ready( function() {
-	$("#block-1").on("click", function(e) {
+$(document).ready(function () {
+	$("#block-1").on("click", function (e) {
 		e.preventDefault();
 		let top = $("#wwd").offset().top;
 		$("html, body").animate({
@@ -7,11 +7,32 @@ $(document).ready( function() {
 		}, 500);
 	});
 
-	$("#block-2").on("click", function(e) {
+	$("#block-2").on("click", function (e) {
 		e.preventDefault();
 		let top = $("#services").offset().top;
 		$("html, body").animate({
 			scrollTop: top
 		}, 500);
+	});
+
+	//////////////////////
+	//									//
+	//		Accordion			//
+	//									//
+	//////////////////////
+
+	$('.services-wwd__text').not(':first').hide();
+	$('.services-wwd__title-wrap').click(function () {
+
+		const findText = $(this).next('.services-wwd__text');
+		const findItem = $(this).closest('.services-wwd__text');
+
+		if (findText.is(':visible')) {
+			findText.slideUp()
+		}
+		else {
+			findItem.find('..services-wwd__text').slideUp();
+			findText.slideDown();
+		}
 	});
 });
