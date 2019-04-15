b7ac7be7b110a9b0cbea8d3fe9cd56167a52ebc6
CunningFox <sergei181989@gmail.com>
2019-04-15 12:19:00 +0300

    add counter to block 'Facts' & add slider to block 'Quote'

--- mogo/src/js/script.js
+++ mogo/src/js/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+
+	//////////////////////
+	//									//
+	//	 Menu-scroll  	//
+	//									//
+	//////////////////////
+
 	$("#block-1").on("click", function (e) {
 		e.preventDefault();
 		let top = $("#wwd").offset().top;
@@ -13,6 +20,24 @@ $(document).ready(function () {
 		$("html, body").animate({
 			scrollTop: top
 		}, 500);
+	});
+
+	//////////////////////
+	//									//
+	//			Facts				//
+	//									//
+	//////////////////////
+
+	$('.facts__number').each(function() {
+		$(this).prop('Counter', 0).animate({
+			Counter:$(this).text()
+		}, {
+			duration: 4000,
+			easing: 'swing',
+			step: function(now) {
+				$(this).text(Math.ceil(now));
+			}
+		});
 	});
 
 	//////////////////////
@@ -43,16 +68,53 @@ $(document).ready(function () {
 	//////////////////////
 
 	const $btnTop = $('.btn-top')
-	$(window).on('scroll', function() {
-		if ($(window).scrollTop() >= 650){
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() >= 650) {
 			$btnTop.fadeIn();
-		}else{
+		} else {
 			$btnTop.fadeOut();
 		}
 	});
 
-	$btnTop.on('click', function() {
-		$('html, body').animate({scrollTop:0}, 900)
+	$btnTop.on('click', function () {
+		$('html, body').animate({ scrollTop: 0 }, 900)
+	});
+
+
+	//////////////////////
+	//									//
+	//		slider				//
+	//									//
+	//////////////////////
+
+	$('.next-btn').click(function () {
+		const currentWrap = $('.quote-wrap.curry');
+		const currentWrapIndex = $('.quote-wrap.curry').index();
+		let nextWrapIndex = currentWrapIndex + 1;
+		let nextWrap = $('.quote-wrap').eq(nextWrapIndex);
+		currentWrap.fadeOut(1000);
+		currentWrap.removeClass('curry');
+
+		if (nextWrapIndex == ($('.quote-wrap:last').index() + 1)) {
+			$('.quote-wrap').eq(0).fadeIn(1000);
+			$('.quote-wrap').eq(0).addClass('curry');
+		} else {
+			nextWrap.fadeIn(1000);
+			nextWrap.addClass('curry');
+		}
+	});
+
+	$('.next-btn').click(function () {
+
+		const currentWrap = $('.quote-wrap.curry');
+		const currentWrapIndex = $('.quote-wrap.curry').index();
+		let prevWrapIndex = currentWrapIndex - 1;
+		let prevWrap = $('.quote-wrap').eq(prevWrapIndex);
+
+		currentWrap.fadeOut(1000);
+		currentWrap.removeClass('curry');
+		prevWrap.fadeIn(1000);
+		prevWrap.addClass('curry');
 	});
 
 });
