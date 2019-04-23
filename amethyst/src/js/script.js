a6a3b3136bb87550b28d5e1f22c8e40c9a15c4cb
CunningFox <sergei181989@gmail.com>
2019-04-23 17:11:55 +0300

    added block facts, our-team, services

--- amethyst/src/js/script.js
+++ amethyst/src/js/script.js
@@ -31,3 +31,25 @@ function paddingHack(items) {
 const paddingHackEl = paddingHack('.img-wrapper');
 
 paddingHackEl.init();
+
+$(document).ready(function () {
+
+	//////////////////////
+	//									//
+	//			Facts				//
+	//									//
+	//////////////////////
+
+	$('.facts__number').each(function () {
+		$(this).prop('Counter', 0).animate({
+			Counter: $(this).text()
+		}, {
+				duration: 4000,
+				easing: 'swing',
+				step: function (now) {
+					$(this).text(Math.ceil(now));
+				}
+			});
+	});
+
+});
