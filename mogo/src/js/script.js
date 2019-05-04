6132527a9cc0f5b22cf3690100bc39d904da6a8d
CunningFox <sergei181989@gmail.com>
2019-05-04 23:22:49 +0300

    fixed project MoGo in map.html & map.scss

--- mogo/src/js/script.js
+++ mogo/src/js/script.js
@@ -1,17 +1,191 @@
-;
-(function () {
-	'use strict'
 
-	// function initMap() {
+	function initMap() {
 
-	// 	var element = document.getElementById('map');
-	// 	var options = {
-	// 		zoom: 16,
-	// 		center: { lat: 49.0765856, lng: 33.4247932 }
-	// 	};
+		// Create a new StyledMapType object, passing it an array of styles,
+		// and the name to be displayed on the map type control.
+		var styledMapType = new google.maps.StyledMapType(
+			[
+				{
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"color": "#242f3e"
+						}
+					]
+				},
+				{
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#746855"
+						}
+					]
+				},
+				{
+					"elementType": "labels.text.stroke",
+					"stylers": [
+						{
+							"color": "#242f3e"
+						}
+					]
+				},
+				{
+					"featureType": "administrative.locality",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#d59563"
+						}
+					]
+				},
+				{
+					"featureType": "poi",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#d59563"
+						}
+					]
+				},
+				{
+					"featureType": "poi.park",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"color": "#263c3f"
+						}
+					]
+				},
+				{
+					"featureType": "poi.park",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#6b9a76"
+						}
+					]
+				},
+				{
+					"featureType": "road",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"color": "#38414e"
+						}
+					]
+				},
+				{
+					"featureType": "road",
+					"elementType": "geometry.stroke",
+					"stylers": [
+						{
+							"color": "#212a37"
+						}
+					]
+				},
+				{
+					"featureType": "road",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#9ca5b3"
+						}
+					]
+				},
+				{
+					"featureType": "road.highway",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"color": "#746855"
+						}
+					]
+				},
+				{
+					"featureType": "road.highway",
+					"elementType": "geometry.stroke",
+					"stylers": [
+						{
+							"color": "#1f2835"
+						}
+					]
+				},
+				{
+					"featureType": "road.highway",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#f3d19c"
+						}
+					]
+				},
+				{
+					"featureType": "transit",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"color": "#2f3948"
+						}
+					]
+				},
+				{
+					"featureType": "transit.station",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#d59563"
+						}
+					]
+				},
+				{
+					"featureType": "water",
+					"elementType": "geometry",
+					"stylers": [
+						{
+							"color": "#17263c"
+						}
+					]
+				},
+				{
+					"featureType": "water",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+							"color": "#515c6d"
+						}
+					]
+				},
+				{
+					"featureType": "water",
+					"elementType": "labels.text.stroke",
+					"stylers": [
+						{
+							"color": "#17263c"
+						}
+					]
+				}
+			],
+			{ name: 'Styled Map' });
 
-	// 	var myMap = new google.maps.Map(element, options);
-	// }
+		// Create a map object, and include the MapTypeId to add
+		// to the map type control.
+		var map = new google.maps.Map(document.getElementById('map'), {
+			center: { lat: 49.0664576, lng: 33.4141739 },
+			zoom: 16,
+			zoomControl: false,
+			mapTypeControl: false,
+			streetViewControl: false,
+			mapTypeControlOptions: {
+				mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+					'styled_map']
+			}
+		});
+
+		//Associate the styled map with the MapTypeId and set it to display.
+		map.mapTypes.set('styled_map', styledMapType);
+		map.setMapTypeId('styled_map');
+	}
+
 
 	$(document).ready(function () {
 
@@ -135,4 +309,3 @@
 
 
 
-})();
