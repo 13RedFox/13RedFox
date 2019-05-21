ebcc13951392794442410bb178b79ce62b70cf76
CunningFox <sergei181989@gmail.com>
2019-05-21 19:04:27 +0300

    cahee: fixed bug on project

--- cahee/src/js/script.js
+++ cahee/src/js/script.js
@@ -2,13 +2,13 @@
 (function () {
 	'use strict'
 
-	function paddingHack(items){
+	function paddingHack(items) {
 		const gridWidth = $(items).parent().width();
-	
-		function setPadding(){
-			$(items).each(function() {
+
+		function setPadding() {
+			$(items).each(function () {
 				const img = $(this).find('img');
-				if(!img[0]) return;
+				if (!img[0]) return;
 				$(this).css({
 					position: 'relative',
 					width: '100%',
@@ -25,25 +25,68 @@
 				});
 			});
 		}
-	
+
 		return {
 			init: setPadding
 		}
 	}
-	
+
 	const paddingHackEl = paddingHack('.img-wrapper');
-	
+
 	paddingHackEl.init();
 
-	
 
+$(document).ready( function() {
 
 	// burger-menu
-
 	$('.mobile').hide();
-
 	$('.burger-menu').on('click', function () {
 		$('.mobile').slideToggle();
-	})
+	});
+
+
+	$("#block1").on("click", function (e) {
+		e.preventDefault();
+		let top = $("#about").offset().top;
+		$("html, body").animate({
+			scrollTop: top
+		}, 500);
+	});
+
+	$("#block2").on("click", function (e) {
+		e.preventDefault();
+		let top = $("#services").offset().top;
+		$("html, body").animate({
+			scrollTop: top
+		}, 1000);
+	});
+
+	$("#block3").on("click", function (e) {
+		e.preventDefault();
+		let top = $("#gallery").offset().top;
+		$("html, body").animate({
+			scrollTop: top
+		}, 2000);
+	});
+
+	$("#block4").on("click", function (e) {
+		e.preventDefault();
+		let top = $("#blog").offset().top;
+		$("html, body").animate({
+			scrollTop: top
+		}, 2000);
+	});
+
+	$("#block5").on("click", function (e) {
+		e.preventDefault();
+		let top = $("#contact").offset().top;
+		$("html, body").animate({
+			scrollTop: top
+		}, 2000);
+	});
+
+});
+
+
 
 })();
