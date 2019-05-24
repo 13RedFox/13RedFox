57362bcc09c23c4891d5867c6d879cd8aad2ad55
CunningFox <sergei181989@gmail.com>
2019-05-24 23:49:58 +0300

    Active-Box: push all file.

--- active-box/app/js/script.js
+++ active-box/app/js/script.js
@@ -42,6 +42,23 @@
 			$(this).toggleClass('btn-active');
 		});
 
+		$(function() {
+			// Owl Carousel
+			var owl = $(".owl-carousel");
+			owl.owlCarousel({
+				items: 1,
+				margin: 10,
+				loop: true,
+				nav: true,
+				autoplay: true,
+				autoplayTimeout: 5000,
+				navText: [
+					'<span class="arrow-owl arrow-left"></span>',
+					'<span class="arrow-owl arrow-right"></span>'
+				],
+			});
+		});
+
 
 	});
 
