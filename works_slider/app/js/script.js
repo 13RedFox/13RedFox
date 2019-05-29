f699c800d3275d4bc3ebfe4b51e6d8157b2cb9b3
CunningFox <sergei181989@gmail.com>
2019-05-29 21:54:52 +0300

    Works_slider: created project & working

---
+++ works_slider/app/js/script.js
@@ -0,0 +1,14 @@
+;
+(function () {
+	'use strict'
+
+	$(document).ready(function () {
+		$('.slick_slider').slick({
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			// autoplay: true,
+			// autoplaySpeed: 2000,
+		});
+	});
+
+})();
