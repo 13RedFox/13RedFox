ce5f369bb4cf9018dc8e227f65d450df376fea09
CunningFox <sergei181989@gmail.com>
2019-06-06 20:40:30 +0300

    DigitalProducts: fixed bug on carousel & created block (OurTeam)

--- digitalproducts/src/js/script.js
+++ digitalproducts/src/js/script.js
@@ -11,6 +11,32 @@
 			$('.burger__line:nth-child(3)').toggleClass('bottom');
 		});
 
+		// Works Owl Carousel
+		$(function () {
+			const owl = $(".works__carousel");
+			owl.owlCarousel({
+				items: 1,
+				loop: true,
+				nav: true,
+				dots: true,
+				navText: [
+					'<span class="arrow-owl arrow-left"></span>',
+					'<span class="arrow-owl arrow-right"></span>'
+				],
+				responsive: {
+					0: {
+						nav: false
+					},
+					768: {
+						nav: false
+					},
+					992: {
+						nav: true
+					}
+				}
+			});
+		});
+
 	});
 
 })();
