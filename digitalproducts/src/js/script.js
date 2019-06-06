1811be7c3eda0ce9982ab3661a1044a979d4bd05
CunningFox <sergei181989@gmail.com>
2019-06-06 23:44:10 +0300

    DigitalProducts: created block (Our-Team) & working..

--- digitalproducts/src/js/script.js
+++ digitalproducts/src/js/script.js
@@ -37,6 +37,36 @@
 			});
 		});
 
+		// OurTeam Owl Carousel
+		$(function () {
+			const owl = $(".our-team__carousel");
+			owl.owlCarousel({
+				items: 3,
+				margin: 10,
+				loop: true,
+				dots: false,
+				nav:true,
+				navText: [
+					'<span class="arrow-owl arrow-left"></span>',
+					'<span class="arrow-owl arrow-right"></span>'
+				],
+				responsive: {
+					0: {
+						nav: false,
+						items: 1
+					},
+					768: {
+						nav: false,
+						items: 2
+					},
+					992: {
+						nav: true,
+						items: 3
+					}
+				}
+			});
+		});
+
 	});
 
 })();
