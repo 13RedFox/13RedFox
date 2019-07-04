94e4d54617cc383d2f7bfe53da0de31729b9e490
Serhii <sergei181989@gmail.com>
2019-07-05 00:08:45 +0300

    Amethyst: working in project

--- amethyst/app/js/script.js
+++ amethyst/app/js/script.js
@@ -12,16 +12,29 @@
 	};
 
 	$(document).ready(function () {
+		//Counter facts
+		$('.facts__number').each(function () {
+			$(this).prop('Counter', 0).animate({
+				Counter: $(this).text()
+			}, {
+					duration: 2000,
+					easing: 'swing',
+					step: function (now) {
+						$(this).text(Math.ceil(now));
+					}
+				});
+		});
 
 		$(function () {
 			// Owl Carousel
-			let owl = $(".owl-carousel");
+			let owl = $(".header-owl-carousel");
 			owl.owlCarousel({
 				items: 1,
 				margin: 10,
 				loop: true,
 				nav: true,
-				autoplayTimeout: 5000,
+				autoplay: true,
+				autoplayTimeout: 3000,
 				navText: [
 					'<span class="arrow-owl arrow-left"></span>',
 					'<span class="arrow-owl arrow-right"></span>'
@@ -50,11 +63,54 @@
 			owl.owlCarousel({
 				items: 1,
 				loop: true,
+				autoplay: true,
+				autoplayTimeout: 3000,
+				responsive: {
+					0: {
+						nav: false,
+						dots: false
+					},
+					768: {
+						nav: false,
+						dots: false
+					},
+					992: {
+						nav: false,
+						dots: true
+					}
+				}
+			});
+		});
+
+		// Testimonials Owl Carousel
+		$(function () {
+			const owl = $(".parteners-owl-carousel");
+			owl.owlCarousel({
+				items: 6,
+				loop: true,
+				margin: 10,
+				dots: false,
+				autoplay: true,
+				autoplayTimeout: 3000,
+				responsive: {
+					0: {
+						items: 1
+					},
+					768: {
+						items: 3
+					},
+					992: {
+						items: 4
+					},
+					1200: {
+						items: 6
+					}
+				}
 			});
 		});
 
 		$(function () {
-
+			// Works gallery
 			$('.works__filter-link').on('click', function (e) {
 				e.preventDefault();
 
