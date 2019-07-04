95604883bd100ba3ee1fd9cb9c687a1456a1aba5
Serhii <sergei181989@gmail.com>
2019-07-04 17:01:20 +0300

    Amethyst: working in project, add blocks(about.html, works.html, benefits.html, testimonials.html

--- amethyst/app/js/script.js
+++ amethyst/app/js/script.js
@@ -9,37 +9,59 @@
 		} else {
 			$(".header__top").removeClass("fixed__top");
 		}
-	}
+	};
 
 	$(document).ready(function () {
 
 		$(function () {
 			// Owl Carousel
-			var owl = $(".owl-carousel");
+			let owl = $(".owl-carousel");
 			owl.owlCarousel({
 				items: 1,
 				margin: 10,
 				loop: true,
 				nav: true,
+				autoplayTimeout: 5000,
 				navText: [
 					'<span class="arrow-owl arrow-left"></span>',
 					'<span class="arrow-owl arrow-right"></span>'
 				],
-				responsive:{
-					320:{
+				responsive: {
+					320: {
 						nav: false,
 						dots: false
 					},
-					768:{
+					768: {
 						nav: false,
 						dots: false
 					},
-					1200:{
+					1200: {
 						loop: true,
 						nav: true
 					}
 				}
 			});
+
+		});
+
+		// Testimonials Owl Carousel
+		$(function () {
+			const owl = $(".testimonials-carousel");
+			owl.owlCarousel({
+				items: 1,
+				loop: true,
+			});
+		});
+
+		$(function () {
+
+			$('.works__filter-link').on('click', function (e) {
+				e.preventDefault();
+
+				$('.works__filter-link').removeClass('active');
+				$(this).addClass('active');
+			});
+
 		});
 
 	});
